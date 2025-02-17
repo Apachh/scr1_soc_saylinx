0470c62fc0c307c7d8e888cd98ef0bdcc7baec58
Mikhail Vasilev <misha.vasilev.201486@gmail.com>
2025-02-17 21:26:21 +0500

    Add custom properties, add instance of scr1 to main file

---
+++ ip/scr1_arch_castom.svh
@@ -0,0 +1,30 @@
+`ifndef SCR1_ARCH_CUSTOM_SVH
+`define SCR1_ARCH_CUSTOM_SVH
+
+// Current FPGA build identificators, can be modified
+`define SCR1_PTFM_SOC_ID            32'h21041500
+`define SCR1_PTFM_BLD_ID            32'h22011203
+`define SCR1_PTFM_CORE_CLK_FREQ     32'd20000000
+
+// Uncomment to select recommended core architecture configurations
+// Default SCR1 FPGA SDK created for RV32IMC_MAX config
+
+// `define SCR1_CFG_RV32IMC_MAX
+//`define SCR1_CFG_RV32IC_BASE
+`define SCR1_CFG_RV32EC_MIN
+
+`define SCR1_ARCH_CUSTOM
+
+// `define SCR1_DBG_EN
+`undef SCR1_TCM_EN
+
+parameter bit [`SCR1_XLEN-1:0]          SCR1_ARCH_RST_VECTOR        = 'hFFFFFF00;   // Reset vector
+parameter bit [`SCR1_XLEN-1:0]          SCR1_ARCH_MTVEC_BASE        = 'hFFFFFF80;   // MTVEC BASE field reset value
+
+parameter bit [`SCR1_DMEM_AWIDTH-1:0]   SCR1_TCM_ADDR_MASK          = 'hFFFF0000;   // TCM mask and size
+parameter bit [`SCR1_DMEM_AWIDTH-1:0]   SCR1_TCM_ADDR_PATTERN       = 'hF0000000;   // TCM address match pattern
+
+parameter bit [`SCR1_DMEM_AWIDTH-1:0]   SCR1_TIMER_ADDR_MASK        = 'hFFFFFFE0;   // Timer mask (should be 0xFFFFFFE0)
+parameter bit [`SCR1_DMEM_AWIDTH-1:0]   SCR1_TIMER_ADDR_PATTERN     = 'hF0040000;   // Timer address match pattern
+
+`endif // SCR1_ARCH_CUSTOM_SVH
--- saylinx_scr1.sv
+++ saylinx_scr1.sv
@@ -1,3 +1,160 @@
-module saylinx_scr1 (
 
+`include "scr1_arch_castom.svh"
+`include "scr1_arch_types.svh"
+`include "scr1_arch_description.svh"
+`include "scr1_memif.svh"
+`include "scr1_ipic.svh"
+
+module saylinx_scr1 (
+    input   logic       CLK_50MHZ,
+
+    output  logic [3:0] LED,
+    input   logic [1:0] KEY,
+
+`ifdef SCR1_DBG_EN  
+    input   logic       JTAG_SRST_N,
+    input   logic       JTAG_TRST_N,
+    input   logic       JTAG_TCK,
+    input   logic       JTAG_TMS,
+    input   logic       JTAG_TDI,
+    output  logic       JTAG_TDO,
+`endif//SCR1_DBG_EN
+
+    // === UART ============================================
+    // output logic                    UART_TXD,    // <- UART
+    // input  logic                    UART_RXD     // -> UART
+);
+
+
+
+logic                               pwrup_rst_n;
+logic                               cpu_clk;
+logic                               extn_rst_in_n;
+logic                               extn_rst_n;
+logic [1:0]                         extn_rst_n_sync;
+logic                               hard_rst_n;
+logic [3:0]                         hard_rst_n_count;
+logic                               soc_rst_n;
+logic                               cpu_rst_n;
+`ifdef SCR1_DBG_EN
+logic                               sys_rst_n;
+`endif // SCR1_DBG_EN
+
+//=======================================================
+//  Resets
+//=======================================================
+assign extn_rst_in_n    = KEY[0]
+`ifdef SCR1_DBG_EN
+                        & JTAG_SRST_N
+`endif // SCR1_DBG_EN
+;
+
+always_ff @(posedge cpu_clk, negedge pwrup_rst_n)
+begin
+    if (~pwrup_rst_n) begin
+        extn_rst_n_sync     <= '0;
+    end else begin
+        extn_rst_n_sync[0]  <= extn_rst_in_n;
+        extn_rst_n_sync[1]  <= extn_rst_n_sync[0];
+    end
+end
+assign extn_rst_n = extn_rst_n_sync[1];
+
+always_ff @(posedge cpu_clk, negedge pwrup_rst_n)
+begin
+    if (~pwrup_rst_n) begin
+        hard_rst_n          <= 1'b0;
+        hard_rst_n_count    <= '0;
+    end else begin
+        if (hard_rst_n) begin
+            // hard_rst_n == 1 - de-asserted
+            hard_rst_n          <= extn_rst_n;
+            hard_rst_n_count    <= '0;
+        end else begin
+            // hard_rst_n == 0 - asserted
+            if (extn_rst_n) begin
+                if (hard_rst_n_count == '1) begin
+                    // If extn_rst_n = 1 at least 16 clocks,
+                    // de-assert hard_rst_n
+                    hard_rst_n          <= 1'b1;
+                end else begin
+                    hard_rst_n_count    <= hard_rst_n_count + 1'b1;
+                end
+            end else begin
+                // If extn_rst_n is asserted within 16-cycles window -> start
+                // counting from the beginning
+                hard_rst_n_count    <= '0;
+            end
+        end
+    end
+end
+
+`ifdef SCR1_DBG_EN
+assign soc_rst_n = sys_rst_n;
+`else
+assign soc_rst_n = hard_rst_n;
+`endif // SCR1_DBG_EN
+
+
+scr1_top_ahb
+i_scr1 (
+        // Common
+        .pwrup_rst_n                (pwrup_rst_n            ),
+        .rst_n                      (hard_rst_n             ),
+        .cpu_rst_n                  (cpu_rst_n              ),
+        .test_mode                  (1'b0                   ),
+        .test_rst_n                 (1'b1                   ),
+        .clk                        (cpu_clk                ),
+        .rtc_clk                    (1'b0                   ),
+`ifdef SCR1_DBG_EN
+        .sys_rst_n_o                (sys_rst_n              ),
+        .sys_rdc_qlfy_o             (                       ),
+`endif // SCR1_DBG_EN
+
+        // Fuses
+        .fuse_mhartid               ('0                     ),
+`ifdef SCR1_DBG_EN
+        .fuse_idcode                (`SCR1_TAP_IDCODE       ),
+`endif // SCR1_DBG_EN
+
+        // IRQ
+`ifdef SCR1_IPIC_EN
+        .irq_lines                  (scr1_irq               ),
+`else
+        .ext_irq                    (scr1_irq             ),
+`endif//SCR1_IPIC_EN
+        .soft_irq                   ('0                     ),
+
+`ifdef SCR1_DBG_EN
+        // Debug Interface - JTAG I/F
+        .trst_n                     (scr1_jtag_trst_n       ),
+        .tck                        (scr1_jtag_tck          ),
+        .tms                        (scr1_jtag_tms          ),
+        .tdi                        (scr1_jtag_tdi          ),
+        .tdo                        (scr1_jtag_tdo_int      ),
+        .tdo_en                     (scr1_jtag_tdo_en       ),
+`endif//SCR1_DBG_EN
+
+        // Instruction Memory Interface
+        .imem_hprot                 (ahb_imem_hprot         ),
+        .imem_hburst                (ahb_imem_hburst        ),
+        .imem_hsize                 (ahb_imem_hsize         ),
+        .imem_htrans                (ahb_imem_htrans        ),
+        .imem_hmastlock             (                       ),
+        .imem_haddr                 (ahb_imem_haddr         ),
+        .imem_hready                (ahb_imem_hready        ),
+        .imem_hrdata                (ahb_imem_hrdata        ),
+        .imem_hresp                 (ahb_imem_hresp         ),
+        // Data Memory Interface
+        .dmem_hprot                 (ahb_dmem_hprot         ),
+        .dmem_hburst                (ahb_dmem_hburst        ),
+        .dmem_hsize                 (ahb_dmem_hsize         ),
+        .dmem_htrans                (ahb_dmem_htrans        ),
+        .dmem_hmastlock             (                       ),
+        .dmem_haddr                 (ahb_dmem_haddr         ),
+        .dmem_hwrite                (ahb_dmem_hwrite        ),
+        .dmem_hwdata                (ahb_dmem_hwdata        ),
+        .dmem_hready                (ahb_dmem_hready        ),
+        .dmem_hrdata                (ahb_dmem_hrdata        ),
+        .dmem_hresp                 (ahb_dmem_hresp         )
 );
